ff23b6ba6bdcadb4ab97b154281a3a6ada638b5a
venkatvikram <venkatvikram1@gmail.com>
2023-05-12 18:29:54 +0530

    12-05-2023
    Changes in the interfaces
    Button  position changes

--- javascript.js.js
+++ javascript.js.js
@@ -1,12 +1,12 @@
 var openButton = document.getElementById('openForm');
-var formPopup = document.getElementById('formPopup');
+var form = document.getElementById('form');
 
 openButton.addEventListener('click', function() {
-  formPopup.style.display = 'block';
+  form.style.display = 'block';
 });
 
-formPopup.addEventListener('submit', function(event) {
+form.addEventListener('submit', function(event) {
   event.preventDefault();
   // Code to process the form data goes here
-  formPopup.style.display = 'none';
+  form.style.display = 'none';
 });
